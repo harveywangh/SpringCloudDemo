17299344af2dfd6bceaebdc2b765bfbd7f50b388
王大航 <Mr_Whang@163.com>
2020-12-01 14:16:35 +0800

    第一次提交

---
+++ back_eurake_server/src/main/java/com/example/kaidun/eurake/config/SpringSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.kaidun.eurake.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author : harvet
+ * @program : com.example.kaidun.eurake.config
+ * @description : SpringSecurity配置
+ **/
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * @Description: 对eureka注册的URL不进行CSRF防御
+     * @Param: [http]
+     * @return: void
+     * @Author: harvey
+     *
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+
+}
--- back_utils/src/main/java/com/example/kaidun/utils/common/vo/BasePageVo.java
+++ back_utils/src/main/java/com/example/kaidun/utils/common/vo/BasePageVo.java
@@ -14,10 +14,12 @@ public class BasePageVo extends BaseRequestVo {
     private Integer nowPage = 1;
     private Integer pageSize = 10;
 
+
+
     @Override
     public void checkParam() throws CommonServiceException {
         if (nowPage == null || nowPage <= 0) {
-            throw new CommonServiceException(404, "页码参数不能为空或者小于0");
+            throw new CommonServiceException(404, "页码参数不能为空或者小于0 1111");
         }
         if (pageSize == null || pageSize <= 0) {
             throw new CommonServiceException(404, "当页条数参数不能为空或者小于0");
